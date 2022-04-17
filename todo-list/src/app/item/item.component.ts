2b2778f4ec024c5b46dca9b8031e2d9f8263e358
alex <hersgory2134@gmail.com>
2022-04-17 21:08:27 +0300

    refactoring

--- todo-list/src/app/item/item.component.ts
+++ todo-list/src/app/item/item.component.ts
@@ -1,27 +1,26 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export interface Book {
-  readonly id : number;
-  readonly name : string
+  readonly id: number;
+  readonly name: string;
 }
 
 @Component({
-  selector: 'app-item',  // <>  тег с таким именем будет испольозоваться
+  selector: 'app-item', // <>  тег с таким именем будет испольозоваться
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
-
 export class ItemComponent {
-    @Input() book!: Book;
-    @Input() isShowOnlyName = false;
-    @Output() onDeleteTask : EventEmitter<void> = new EventEmitter<void>();
-    @Output() onCloseTask : EventEmitter<void> = new EventEmitter<void>();
+  @Input() book!: Book;
+  @Input() isShowOnlyName = false;
+  @Output() onDeleteTask: EventEmitter<void> = new EventEmitter<void>();
+  @Output() onCloseTask: EventEmitter<void> = new EventEmitter<void>();
 
-    delete(): void {
-      this.onDeleteTask.emit();
-    }
+  delete(): void {
+    this.onDeleteTask.emit();
+  }
 
-    close(): void {
-      this.onCloseTask.emit();
-    }
+  close(): void {
+    this.onCloseTask.emit();
+  }
 }
